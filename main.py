8d428e9f4d07d630574b3bab10c4972dde9e7da3
Jani Kaartinen <jkaart@gmail.com>
2024-05-14 12:06:43 +0300

    Pelaaja ja Hirssipuu luokkkien aloitus

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("Hello World!")
+import random
+
+class Pelaaja:
+    def __init__(self) -> None:
+        self.nimi = ""
+        
+
+    def uusi_pelaaja(self):
+        pass
+class Hirsipuu:
+    def __init__(self) -> None:
+        self.pelaajat = []
+
+    def uusi_peli(self):
+        pass
+    
+    def lisaa_pelaaja(self):
+        #self.nimi = input("Anna pelaajan nimi: ")
+        pelaaja = Pelaaja("aa")
+
+        #pelaaja.uusi_pelaaja()
+        self.pelaajat.append(pelaaja)
+
+
+
+        
+
+
+hirsipuu = Hirsipuu
+hirsipuu.lisaa_pelaaja()
